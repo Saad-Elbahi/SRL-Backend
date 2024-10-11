2e8e512e9915adde2446f17dacd2b627474e9be6
Saad-Elbahi <elbahi.saademsi.1@gmail.com>
2024-10-11 15:10:31 +0100

    UPDATE 11-10-2024

---
+++ sr-logistique/sr-mouvementv/src/main/java/ma/srmanager/srmouvementv/controllers/ClientController.java
@@ -0,0 +1,53 @@
+package ma.srmanager.srmouvementv.controllers;
+
+import ma.srmanager.srmouvementv.dto.ClientDTO;
+import ma.srmanager.srmouvementv.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/clientApi")
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/createClient")
+    public ResponseEntity<ClientDTO> createClient(@RequestBody ClientDTO clientDTO) {
+        ClientDTO createdClient = clientService.createClient(clientDTO);
+        return ResponseEntity.ok(createdClient);
+    }
+
+    @PutMapping("/updateClient")
+    public ResponseEntity<ClientDTO> updateClient( @RequestBody ClientDTO clientDTO) {
+        ClientDTO updatedClient = clientService.updateClient(clientDTO.getId(), clientDTO);
+        if (updatedClient != null) {
+            return ResponseEntity.ok(updatedClient);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/allClient")
+    public ResponseEntity<List<ClientDTO>> getAllClients() {
+        List<ClientDTO> clients = clientService.getAllClients();
+        return ResponseEntity.ok(clients);
+    }
+
+    @GetMapping("/clientById")
+    public ResponseEntity<ClientDTO> getClientById(@RequestBody ClientDTO clientDTO) {
+        Optional<ClientDTO> client = clientService.getClientById(clientDTO.getId());
+        return client.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/deleteClient")
+    public ResponseEntity<Void> deleteClient(@RequestBody ClientDTO clientDTO) {
+        clientService.deleteClient(clientDTO.getId());
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ sr-logistique/sr-mouvementv/src/main/java/ma/srmanager/srmouvementv/dto/ClientDTO.java
@@ -0,0 +1,19 @@
+package ma.srmanager.srmouvementv.dto;
+
+import lombok.Data;
+
+@Data
+public class ClientDTO {
+    private Long id;
+    private String name;
+    private String address;
+    private  String  email;
+    private String  ville;
+    private  String  fiscalId;
+    private  String  rc;
+    private  String  cnss;
+    private  String  rib;
+    private String grantName;
+    private String gerantTelephone;
+    private String gerantEmail;
+}
